456ed59c1fc94a66a3b88c95f23e47774ad6e64a
Adas Ginevicius <adas.ginevicius@prism.training>
2025-08-08 13:03:32 +0100

    Airflow dag has been created and runningsuccesfully extracting top 10 crypto coins and loads it into big query. Dim and factables have been updating making sure coin_id doesnt contain duplicates

--- crypto_dbt/models/dim_coin.sql
+++ crypto_dbt/models/dim_coin.sql
@@ -2,8 +2,20 @@
     materialized='table'
 )}}
 
+WITH ranked AS (
+    SELECT
+        id AS coin_id,
+        name,
+        symbol,
+        fetched_at,
+        row_number() OVER (PARTITION BY id ORDER BY fetched_at DESC) AS rn
+    FROM {{source('crypto_data', 'top_10_prices')}}
+)
+
 SELECT DISTINCT
-    id as coin_id,
+    coin_id,
     name,
-    symbol
-FROM {{source('crypto_data', 'top_10_prices')}}
+    symbol,
+    fetched_at
+FROM ranked
+WHERE rn = 1
--- crypto_dbt/models/fact_prices.sql
+++ crypto_dbt/models/fact_prices.sql
@@ -4,11 +4,25 @@
 
 {% do ref('dim_coin') %}
 
+WITH ranked AS (
+    SELECT
+        id AS coin_id,
+        current_price,
+        market_cap,
+        price_change_24h,
+        price_change_percentage_24h,
+        fetched_at,
+        date(fetched_at) AS price_date,
+        row_number() OVER (PARTITION BY id ORDER BY fetched_at DESC) AS rn
+    FROM {{ source('crypto_data', 'top_10_prices') }}
+)
+
 SELECT
-    id AS coin_id,
+    coin_id,
     current_price,
     market_cap,
     price_change_24h,
     price_change_percentage_24h AS price_change_pct_24h,
-    DATE(fetched_at) AS price_date
-FROM {{source('crypto_data', 'top_10_prices')}}
+    price_date
+FROM ranked
+WHERE rn = 1
